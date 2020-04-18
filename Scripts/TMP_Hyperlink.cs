d9d1d1b87f6180ba37cbc058b96dc70b8f1a363e
baba_s <shotababa1206@gmail.com>
2020-04-18 12:45:21 +0900

    fix

--- Scripts/TMP_Hyperlink.cs
+++ Scripts/TMP_Hyperlink.cs
@@ -7,33 +7,42 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-namespace KoganeUnityLib
+namespace UniTextMeshProHyperlink
 {
 	[RequireComponent( typeof( TextMeshProUGUI ) )]
 	public class TMP_Hyperlink : MonoBehaviour, IPointerClickHandler
 	{
-		public Action<string> mOnClick;
+		public event Action<string> OnClick;
 
 		public void OnPointerClick( PointerEventData e )
 		{
-			Debug.Log( "aaa" );
 			var text   = GetComponent<TextMeshProUGUI>();
 			var pos    = Input.mousePosition;
 			var camera = text.canvas.worldCamera;
 			var index  = TMP_TextUtilities.FindIntersectingLink( text, pos, camera );
 
 			if ( index == -1 ) return;
-
+			
 			var linkInfo = text.textInfo.linkInfo[ index ];
 			var url      = linkInfo.GetLinkID();
 
-			Debug.Log( "aaa" );
-			mOnClick?.Invoke( url );
+			if ( OnClick != null )
+			{
+				OnClick( url );
+				return;
+			}
 
 #if !UNITY_EDITOR && UNITY_WEBGL
 			OpenToBlankWindow( url );
 #else
 			Application.OpenURL( url );
+#endif
+		}
+
+		public static void OpenURLInWebGL( string url )
+		{
+#if !UNITY_EDITOR && UNITY_WEBGL
+			OpenToBlankWindow( url );
 #endif
 		}
 
